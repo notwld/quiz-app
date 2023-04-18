c29f0e16e6a000d5489205f9d254cb1c75226a55
notwld <mwfarrukh@gmail.com>
2023-04-18 17:33:07 +0500

    added select option functionality

--- app/src/main/java/com/example/quiz/ui/QuestionActivity.kt
+++ app/src/main/java/com/example/quiz/ui/QuestionActivity.kt
@@ -1,17 +1,21 @@
 package com.example.quiz.ui
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import androidx.core.content.ContextCompat
 import com.example.quiz.R
 import com.example.quiz.models.Question
 import com.example.quiz.utils.Constants
 
-class QuestionActivity : AppCompatActivity() {
+class QuestionActivity : AppCompatActivity(), View.OnClickListener
+{
     private var currentPos = 1
+    private var selectedPos = 0
     private lateinit var questionList: MutableList<Question>
 
     private lateinit var progressBar: ProgressBar
@@ -22,6 +26,8 @@ class QuestionActivity : AppCompatActivity() {
     private lateinit var op2: TextView
     private lateinit var op3: TextView
     private lateinit var op4: TextView
+
+    private lateinit var submitBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -36,10 +42,19 @@ class QuestionActivity : AppCompatActivity() {
         progressText = findViewById(R.id.currentQuestion)
         questionTxt = findViewById(R.id.question)
         flagImg = findViewById(R.id.flagImg)
+
         op1 = findViewById(R.id.opt1)
         op2 = findViewById(R.id.opt2)
         op3 = findViewById(R.id.opt3)
         op4 = findViewById(R.id.opt4)
+        submitBtn = findViewById(R.id.ansSubBtn)
+
+        op1.setOnClickListener(this)
+        op2.setOnClickListener(this)
+        op3.setOnClickListener(this)
+        op4.setOnClickListener(this)
+        
+        submitBtn.setOnClickListener(this)
 
         setQuestion()
     }
@@ -48,7 +63,7 @@ class QuestionActivity : AppCompatActivity() {
         val q = questionList[currentPos-1]
         flagImg.setImageResource(q.img)
         progressBar.progress = currentPos
-        progressText.text = "$currentPos/${progressBar.max}"
+        progressText.text = "$currentPos/${questionList.size}"
 
         questionTxt.text = q.question
         op1.text = q.option1
@@ -56,6 +71,62 @@ class QuestionActivity : AppCompatActivity() {
         op3.text = q.option3
         op4.text = q.option4
 
+        if (currentPos==questionList.size){
+            submitBtn.text = "Finish"
+        }
+        else{
+            submitBtn.text = "Submit"
+        }
 
+    }
+
+    private fun resetOption(){
+        var options = mutableListOf<TextView>()
+        options.add(op1)
+        options.add(op2)
+        options.add(op3)
+        options.add(op4)
+
+        for(option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.border
+            )
+        }
+    }
+
+    private fun selectOption(txtView:TextView,optionId:Int){
+        resetOption()
+
+        selectedPos = optionId
+
+        txtView.setTextColor(Color.parseColor("#363A43"))
+        txtView.setTypeface(txtView.typeface,Typeface.BOLD)
+        txtView.background =  ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_item
+        )
+
+    }
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.opt1 -> {
+                selectOption(op1,1)
+            }
+            R.id.opt2 -> {
+                selectOption(op2,2)
+            }
+            R.id.opt3 -> {
+                selectOption(op3,3)
+            }
+            R.id.opt4 -> {
+                selectOption(op4,4)
+            }
+            R.id.ansSubBtn -> {
+                Toast.makeText(this@QuestionActivity,"You Clicked!",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }

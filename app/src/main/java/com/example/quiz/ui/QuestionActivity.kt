a2b264a4ac08737c3e0e42c4c4d4aa41be21ed71
notwld <mwfarrukh@gmail.com>
2023-04-18 18:11:35 +0500

    added check correct/wrong answer fun

--- app/src/main/java/com/example/quiz/ui/QuestionActivity.kt
+++ app/src/main/java/com/example/quiz/ui/QuestionActivity.kt
@@ -12,10 +12,12 @@ import com.example.quiz.R
 import com.example.quiz.models.Question
 import com.example.quiz.utils.Constants
 
-class QuestionActivity : AppCompatActivity(), View.OnClickListener
-{
-    private var currentPos = 1
-    private var selectedPos = 0
+class QuestionActivity : AppCompatActivity(), View.OnClickListener {
+    private var questionCounter = 1
+    private var selectedAns = 0
+    private var answered = false
+
+
     private lateinit var questionList: MutableList<Question>
 
     private lateinit var progressBar: ProgressBar
@@ -28,6 +30,7 @@ class QuestionActivity : AppCompatActivity(), View.OnClickListener
     private lateinit var op4: TextView
 
     private lateinit var submitBtn: Button
+    private lateinit var currentQuestion: Question
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -53,17 +56,19 @@ class QuestionActivity : AppCompatActivity(), View.OnClickListener
         op2.setOnClickListener(this)
         op3.setOnClickListener(this)
         op4.setOnClickListener(this)
-        
+
         submitBtn.setOnClickListener(this)
 
         setQuestion()
     }
 
-    private fun setQuestion(){
-        val q = questionList[currentPos-1]
+    private fun setQuestion() {
+        resetOption()
+
+        val q = questionList[questionCounter - 1]
         flagImg.setImageResource(q.img)
-        progressBar.progress = currentPos
-        progressText.text = "$currentPos/${questionList.size}"
+        progressBar.progress = questionCounter
+        progressText.text = "$questionCounter/${questionList.size}"
 
         questionTxt.text = q.question
         op1.text = q.option1
@@ -71,23 +76,24 @@ class QuestionActivity : AppCompatActivity(), View.OnClickListener
         op3.text = q.option3
         op4.text = q.option4
 
-        if (currentPos==questionList.size){
+        if (questionCounter == questionList.size) {
             submitBtn.text = "Finish"
-        }
-        else{
+        } else {
             submitBtn.text = "Submit"
         }
-
+        questionCounter++
+        currentQuestion = q
+        answered = false
     }
 
-    private fun resetOption(){
+    private fun resetOption() {
         var options = mutableListOf<TextView>()
         options.add(op1)
         options.add(op2)
         options.add(op3)
         options.add(op4)
 
-        for(option in options){
+        for (option in options) {
             option.setTextColor(Color.parseColor("#7A8089"))
             option.typeface = Typeface.DEFAULT
             option.background = ContextCompat.getDrawable(
@@ -97,36 +103,80 @@ class QuestionActivity : AppCompatActivity(), View.OnClickListener
         }
     }
 
-    private fun selectOption(txtView:TextView,optionId:Int){
+    private fun selectOption(txtView: TextView, optionId: Int) {
         resetOption()
 
-        selectedPos = optionId
+        selectedAns = optionId
 
         txtView.setTextColor(Color.parseColor("#363A43"))
-        txtView.setTypeface(txtView.typeface,Typeface.BOLD)
-        txtView.background =  ContextCompat.getDrawable(
+        txtView.setTypeface(txtView.typeface, Typeface.BOLD)
+        txtView.background = ContextCompat.getDrawable(
             this,
             R.drawable.selected_item
         )
 
     }
+
     override fun onClick(view: View?) {
-        when(view?.id){
+        when (view?.id) {
             R.id.opt1 -> {
-                selectOption(op1,1)
+                selectOption(op1, 1)
             }
             R.id.opt2 -> {
-                selectOption(op2,2)
+                selectOption(op2, 2)
             }
             R.id.opt3 -> {
-                selectOption(op3,3)
+                selectOption(op3, 3)
             }
             R.id.opt4 -> {
-                selectOption(op4,4)
+                selectOption(op4, 4)
             }
             R.id.ansSubBtn -> {
-                Toast.makeText(this@QuestionActivity,"You Clicked!",Toast.LENGTH_LONG).show()
+//                Toast.makeText(this@QuestionActivity,"You Clicked!",Toast.LENGTH_LONG).show()
+                if (!answered) {
+                    checkAnswer()
+                } else {
+                    setQuestion()
+                }
             }
         }
+    }
+
+    private fun checkAnswer() {
+        answered = true
+        if (selectedAns == currentQuestion.correctAnswer) {
+            when (selectedAns) {
+                1 -> {
+                    op1.background = ContextCompat.getDrawable(this, R.drawable.correct)
+                }
+                2 -> {
+                    op2.background = ContextCompat.getDrawable(this, R.drawable.correct)
+                }
+                3 -> {
+                    op3.background = ContextCompat.getDrawable(this, R.drawable.correct)
+                }
+                4 -> {
+                    op4.background = ContextCompat.getDrawable(this, R.drawable.correct)
+                }
+
+
+            }
+        } else {
+            when (selectedAns) {
+                1 -> {
+                    op1.background = ContextCompat.getDrawable(this, R.drawable.wrong)
+                }
+                2 -> {
+                    op2.background = ContextCompat.getDrawable(this, R.drawable.wrong)
+                }
+                3 -> {
+                    op3.background = ContextCompat.getDrawable(this, R.drawable.wrong)
+                }
+                4 -> {
+                    op4.background = ContextCompat.getDrawable(this, R.drawable.wrong)
+                }
+            }
+        }
+        submitBtn.text = "Next"
     }
 }
